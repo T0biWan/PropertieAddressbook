53de83522e92a0064d19aa664332347eb2548e0b
T0biWan <Tobi.Klatt@web.de>
2015-06-08 18:54:31 +0200

    2b

---
+++ src/exceptions/InvalidContactException.java
@@ -0,0 +1,9 @@
+package exceptions;
+
+public class InvalidContactException extends Exception {
+	
+	public InvalidContactException(String message) {
+		super(message);
+	}
+
+}
--- src/klassen/ObservableContactDetails.java
+++ src/klassen/ObservableContactDetails.java
@@ -1,5 +1,14 @@
 package klassen;
 
-public class ObservableContactDetails extends ContactDetails {
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.StringProperty;
 
+public class ObservableContactDetails extends ContactDetails {
+	// Attribute
+	private StringProperty	vorname;
+	private StringProperty	nachname;
+	private StringProperty	adresse;
+	private IntegerProperty	telefonnummer;
+	private StringProperty	mail;
+	
 }
