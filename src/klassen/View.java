064e166cabbe672452634996bc78c31f63dcd5fe
Inga <inga.schwarze2010@gmail.com>
2015-06-11 16:44:40 +0200

    view

--- src/klassen/View.java
+++ src/klassen/View.java
@@ -1,31 +1,21 @@
 package klassen;
 	
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
 import javafx.scene.layout.BorderPane;
+
 
 public class View extends Application {
 	//Attribute
 	Control control = new Control();
-	TableView <ObservableContactDetails> 			tabelleFürKontakte		= new TableView();
 	
-	
-	//Methoden
 	@Override
 	public void start(Stage stage) {
-		initTabelle();
 		
-		//Pane
-		BorderPane pane = new BorderPane();
-		pane.setLeft(tabelleFürKontakte);
 		
 		// SCENE
-		Scene scene = new Scene(pane);
+		Scene scene = new Scene(control.tabelleFürKontakte);
 
 		// STAGE
 		stage.setScene(scene);
@@ -35,31 +25,4 @@ public class View extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
-	public void initTabelle () {
-		//Binding
-		tabelleFürKontakte.setItems(control.oList);
-		
-		//Spalten erzeugen
-		TableColumn<ObservableContactDetails, String> vornameSpalte = new TableColumn<>("Vorname");
-		TableColumn<ObservableContactDetails, String> nachnameSpalte = new TableColumn<>("Nachname");
-		TableColumn<ObservableContactDetails, String> adresseSpalte = new TableColumn<>("Adresse");
-		TableColumn<ObservableContactDetails, String> telefonnummerSpalte = new TableColumn<>("Telefonnummer");
-		TableColumn<ObservableContactDetails, String> mailSpalte = new TableColumn<>("E-Mail");
-		
-		//Spalten an Tabelle übergeben
-		tabelleFürKontakte.getColumns().addAll(vornameSpalte, nachnameSpalte, adresseSpalte, telefonnummerSpalte, mailSpalte);
-		
-		//Spalten mit Inhalt füllen
-		vornameSpalte.setCellValueFactory(e -> e.getValue().vornameProperty());
-		nachnameSpalte.setCellValueFactory(e -> e.getValue().nachnameProperty());
-		adresseSpalte.setCellValueFactory(e -> e.getValue().adresseProperty());
-		telefonnummerSpalte.setCellValueFactory(e -> e.getValue().telefonnummerProperty());
-		mailSpalte.setCellValueFactory(e -> e.getValue().mailProperty());
-		
-		//??
-//		tabelleFürKontakte.setRowFactory(c -> new RowFactory());
-		tabelleFürKontakte.setEditable(true);
-	}
-	
 }
