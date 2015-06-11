829999946d63a60f778dbb44bb623a334822f29b
Inga <inga.schwarze2010@gmail.com>
2015-06-11 16:46:35 +0200

    kp

--- src/klassen/Main.java
+++ src/klassen/Main.java
@@ -1,12 +1,26 @@
 package klassen;
 
-import javafx.scene.control.TableView;
+import klassen.ContactDetails;
 
 public class Main {
+	
+
 	public static void main(String[] args) {
-		Control c = new Control();
+		//Attribute
+		RandomStringGenerator rsg = new RandomStringGenerator();
 		
-		ObservableContactDetails[] kontakte = c.pAddressBook.search("");
+		//Liste mit Inhalt füllen
+		for(int i = 0; i < 10; i++){
+//			ObservableContactDetails ocb = new ObservableContactDetails(rsg.generateRandomString(5), rsg.generateRandomString(5), rsg.generateRandomString(10), rsg.generateRandomInt(10), rsg.generateRandomString(10));
+			String vorname = rsg.generateRandomString(5);
+			String nachname = rsg.generateRandomString(5);
+			String adresse = rsg.generateRandomString(5);
+			int telefonnummer = rsg.generateRandomInt(50000);
+			String mail = rsg.generateRandomString(5);
+//			ObservableContactDetails ocb = new ObservableContactDetails(vorname, nachname, adresse, telefonnummer, mail);
+		}
 		
+		//View starten
 	}
+
 }
